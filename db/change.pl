bed9e5d127d4716e26aca5cc53894a3082a03b27
Clément MILLET <clement.millet@hotmail.fr>
2015-01-23 17:12:05 +0100

    Write code for security, not tested and activated yet

--- db/change.pl
+++ db/change.pl
@@ -6,9 +6,13 @@
 #
 # inputs:
 #     id : of the solr object (required)
+#     token & token timeout : ref to the user that make the request 
+#                             to check if he got a right to do it
 #     + any fields
 #
 # notes: do an explicit commit
+#
+# TODO ENABLE SECURITY !!
 #
 # Contributors:
 #   - Arnaud Gaudinat : 11/08/2014
@@ -56,8 +60,13 @@ if (Config::Simple->error()) {
 else {
     my $deb_mod = $cfg->param('debug');
 
+    #If script call via POST
+    if($q->param('POSTDATA')){
+        my @var = $json->decode($$cgi{'POSTDATA'});
+        $cgi = $var[0];
+    }
 
-    if ($q->param('id')) {
+    if ($$cgi{'id'}) {
         foreach my $k (keys %$cgi) {
             if ($k ne 'callback') {
                 if ($k eq 'id') {
@@ -69,8 +78,27 @@ else {
                 elsif($k eq 'password_hash'){
                     $query .= '"password_s":{"set":"'.$$cgi{$k}.'"},';
                 }
-                elsif($k eq 'password_s'){
-                    $query .= '"password_s":{"set":"'.md5_hex($$cgi{$k}).'"},';
+                elsif($k eq 'source_id_ss'){
+                    my $t='';
+                    my @test = split(',', $$cgi{$k});
+                    foreach my $ve (@test){
+                        $t .= '"'.$ve.'",';
+                    }
+                    #Remove the last coma to prevent error
+                    chop($t);
+                    $query .= '"source_id_ss":{"set":['.$t.']},';
+                }
+                elsif($k eq 'tags_ss'){
+                    my $t='';
+                    my @test = split(',', $$cgi{$k});
+                    foreach my $ve (@test){
+                        #Trim spaces before and after
+                        $ve =~ s/^\s+|\s+$//g;
+                        $t .= '"'.$ve.'",';
+                    }
+                    #Remove the last coma to prevent error
+                    chop($t);
+                    $query .= '"tags_ss":{"set":['.$t.']},';
                 }
                 else{
                     $query .= '"'.$k.'":{"set":"'.$$cgi{$k}.'"},';
@@ -81,51 +109,86 @@ else {
         #$query .= '"revision":{"inc":1}'."\n";
         $json_text = '{'.$query.'}';
         print $json_text;
-
     }
     else{
-        if($q->param('POSTDATA')){
-            my @var = $json->decode($$cgi{'POSTDATA'});
-            foreach my $k (keys $var[0]) {
-                if ($k ne 'callback') {
-                    if ($k eq 'id') {
-                        $query .= '"id":"'.$var[0]{$k}.'",';
-                    }
-                    elsif($k eq 'password_hash'){
-                        $query .= '"password_s":{"set":"'.$var[0]{$k}.'"},';
-                    }
-                    elsif($k eq 'password_s'){
-                        $query .= '"password_s":{"set":"'.md5_hex($var[0]{$k}).'"},';
-                    }
-                    else{
-                        $query .= '"'.$k.'":{"set":"'.$var[0]{$k}.'"},';
-                    }
-                }
-            }
-            $query .= '"updating_dt":{"set":"'.$str_now.'"}';
-            #$query .= '"revision":{"inc":1}'."\n";
-            $json_text = '{'.$query.'}';
-            print $json_text;
-        }
-        else{
-
-            push @{$perl_response{'error'}},'atomic change ID is missing';
-        }
+        push @{$perl_response{'error'}},'atomic change ID is missing';
     }
+    # if(!$$cgi{'token'} || !$$cgi{'token_timeout'}) {
+    #     push @{$perl_response{'error'}},'No user identification';
+    # }
 
 
-    if ($q->param('callback')) {
-        $callback    = $q->param('callback');
+    # if ($q->param('callback')) {
+    #     $callback    = $q->param('callback');
+    # }
+    if ($$cgi{'callback'}) {
+        $callback    = $$cgi{'callback'};
     }
 
 
     if (!(exists $perl_response{'error'})) {
 
-
          # init user_agent
         my $ua = LWP::UserAgent->new;
         $ua->timeout(10);
-        $ua->env_proxy;
+        # $ua->env_proxy;
+
+        # my $response_1 = $ua->get($cfg->param('webso_services').
+        #     uri_encode('/db/get.pl?'.
+        #         $cfg->param('db_type').'='.$cfg->param('t_user').
+        #         '&'.$cfg->param('db_token').'='.$$cgi{'token'}.
+        #         '&'.$cfg->param('db_token_timeout').'='.$$cgi{'token_timeout'}));
+
+        # if ($response_1->is_success) {
+        #     $response_text_1 = $json->decode($response_1->decoded_content);
+        #     if($response_text_1->{success}->{response}->{numFound} eq 1){
+        #         #check if the user is the owner of what he update
+        #         my $response_2 = $ua->get($cfg->param('webso_services').
+        #             uri_encode('/db/get.pl?'.
+        #                 $cfg->param('id').'='.$$cgi{'id'}.
+        #                 '&'.$cfg->param('db_user').':'.$response_text_1->{success}->{response}->{docs}[0]->{id}));
+        #             ));
+        #         if ($response_2->is_success) {
+        #             my $response_text_2 = $json->decode($response_2->decoded_content);
+        #             if($response_text_2->{success}->{response}->{numFound} eq 1){
+        #                 #Make the update
+        #                 my $req = HTTP::Request->new(
+        #                     POST => $cfg->param('ws_db').'update?commit=true'
+        #                 );
+        #                 $req->content_type('application/json');
+        #                 $req->content('['.$json_text.']');
+
+        #                 my $res = $ua->request($req);
+
+
+        #                 if ($res->is_success) {
+        #                     $perl_response{success} = $json->decode($res->content);
+        #                 }
+        #                 else {
+        #                     $perl_response{'error'} = 'sources server or service: '.$res->code;
+        #                     if ($deb_mod) {
+        #                         $perl_response{'debug_msg'} = $res->message;
+        #                     }
+        #                 }
+        #             }
+        #             else{
+        #                 $perl_response{error} = "You don't have the rights to download this file.";
+        #             }
+        #         }else{
+        #             $perl_response{error} = "sources server or service2: ".$response_2->code;
+        #             if ($deb_mod) {
+        #                 $perl_response{debug_msg} = $response_2->message;
+        #             }
+        #         }
+        #     }else{
+        #         $perl_response{error} = "No account available";
+        #     }
+        # }else{
+        #     $perl_response{error} = "sources server or service1: ".$response_1->code;
+        #     if ($deb_mod) {
+        #         $perl_response{debug_msg} = $response_1->message;
+        #     }
+        # }
 
         my $req = HTTP::Request->new(
             POST => $cfg->param('ws_db').'update?commit=true'
